245f062bf516c77a94ccca824ae7708d20e9e36c
luyunyyyyy <516537801@qq.com>
2018-05-16 11:06:34 +0800

    1. 重构了一下代码，把main中的逻辑部分提取出来。 2. main函数会作为一个方法的测试和驱动函数。 3. 后续会生成一个主要方法，如 FrontEnd.run的方法，作为输入法模块的入口

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,28 +4,45 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Main {
-    static Logger logger = Logger.getLogger(Main.class);
+    private static Logger logger = Logger.getLogger(Main.class);
     public static void main(String[] args) {
         logger.info("调用 main ");
         Scanner sc = new Scanner(System.in);
-        Pattern pinyinPattern = Pattern.compile("^[A-Za-z]+$");
-        String input;
-        while (true) {
-            System.out.print("input:");
-            input = sc.next();
-            logger.debug("input : "+input);
-
-            if(input.equals("q")){
-                logger.debug("quit");
-                break;
-            }
-            if(pinyinPattern.matcher(input).matches()){
-                logger.info("input is pinyin");
-            }
-        }
+        runFrontEnd(sc);
 
 
         logger.info("输入法运行结束 ");
 
+    }
+
+    private static void runFrontEnd(Scanner sc) {
+        Pattern pinyinPattern = Pattern.compile("^[A-Za-z]+$");
+        String input, result;
+        while (true) {
+            System.out.print("input:");
+            input = sc.next();
+            logger.debug("input : " + input);
+
+            if (input.equals("q")) {
+                logger.debug("quit");
+                break;
+            }
+            if (pinyinPattern.matcher(input).matches()) {
+                logger.debug("input is pinyin");
+
+                result = setResult(input);
+            } else {
+                logger.debug("input is not pinyin");
+                break;
+            }
+
+            if (result != null) {
+                System.out.println("result : " + result);
+            }
+        }
+    }
+
+    private static String setResult(String input) {
+        return input;
     }
 }
