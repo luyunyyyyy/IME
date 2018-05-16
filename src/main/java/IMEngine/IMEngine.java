08eec63e9154e0e166af4ae6c9988c09a679f371
luyunyyyyy <516537801@qq.com>
2018-05-16 15:09:05 +0800

    创建了一个 Engine 作为核心算法的实现和组织类 可以一开始直接从Main中调用Engine的handleInput方法 后续再使用FrontEnd中转

---
+++ src/main/java/IMEngine/IMEngine.java
@@ -0,0 +1,21 @@
+package IMEngine;
+
+import FrontEnd.FrontEndResult;
+import FrontEnd.FrontEndResultBase;
+
+public class IMEngine {
+    public IMEngine() {
+
+    }
+
+    public void init() {
+
+    }
+
+    public FrontEndResultBase handleInput(String input) {
+        FrontEndResultBase frontEndResultBase = new FrontEndResult();
+
+
+        return frontEndResultBase;
+    }
+}
