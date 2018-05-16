a5ced1c1535dbbf29639c0f56490b74130219424
luyunyyyyy <516537801@qq.com>
2018-05-16 15:23:46 +0800

    该接口表示引擎输出的结果，可以直接送到驱动程序，也可以通过FrontEnd来中转。

---
+++ src/main/java/FrontEnd/FrontEndResultBase.java
@@ -0,0 +1,15 @@
+package FrontEnd;
+
+import java.util.List;
+
+/*
+
+ */
+public interface FrontEndResultBase {
+    List<String> getAllResult();
+
+    String getMaxResult();
+
+    void setResult(List<String> result);
+
+}
