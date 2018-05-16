2576fd3d3cde652d404418f258199d2d5ecdc9b5
luyunyyyyy <516537801@qq.com>
2018-05-16 11:21:35 +0800

    增加了一个基于常量的获取配置的类

---
+++ src/main/java/Config/SimpleConfig.java
@@ -0,0 +1,11 @@
+package Config;
+
+public final class SimpleConfig {
+
+    private SimpleConfig() {
+        // restrict instantiation
+    }
+
+    public static final double PI = 3.14159;
+    public static final double CANDIDATE_WORDS_COUNT = 15;
+}
