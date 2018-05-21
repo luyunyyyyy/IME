76f0feea0c7fa5ac4b34511c1a4f9555868d7e0f
luyunyyyyy <516537801@qq.com>
2018-05-21 15:17:01 +0800

    # #1 完成一个简单的拼音切分标注功能
    基于之前的 正则实现的拼音切分功能, 把切分出来的结果标注上类别 (全拼|简拼|其他)
    
    在配置中, 硬编码进去拼音的所有组合和所有前缀. 作为切分的根据
    
    后续需要完善一个配置读取的模块. #5

--- src/main/java/Config/ConfigService.java
+++ src/main/java/Config/ConfigService.java
@@ -1,4 +1,6 @@
 package Config;
 
 public interface ConfigService {
+//    Set<String> getCompleteSpellSet();//COMPLETESPELL
+//    Set<String> getSimpleSpellSet();//SIMPLESPELL
 }
--- src/main/java/Util/PinyinSplit.java
+++ src/main/java/Util/PinyinSplit.java
@@ -1,10 +1,9 @@
 package Util;
 
+import Config.SimpleConfig;
 import org.apache.log4j.Logger;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,10 +11,10 @@ public class PinyinSplit {
     private static HashSet<String> hashSet;
     private static Logger logger = Logger.getLogger(PinyinSplit.class);
 
-    public static String split(String input) {
-        logger.debug("split 方法被调用");
-        return input;
-    }
+//    public static String split(String input) {
+//        logger.debug("split 方法被调用");
+//        return input;
+//    }
 
     /*
     这个方法会在beijing 切分后变成 bei\sjing\s
@@ -28,24 +27,52 @@ public class PinyinSplit {
 
     public static String splitSpell(String s) {
         String regEx = "[^aoeiuv]?h?[iuv]?(ai|ei|ao|ou|er|ang?|eng?|ong|a|o|e|i|u|ng|n)?";
-        int tag = 0;
-        String spell = "";
-        List<String> tokenResult = new LinkedList<String>();
+        int tag;
+        StringBuilder spell = new StringBuilder();
+        LinkedList tokenResult = new LinkedList();
         for (int i = s.length(); i > 0; i = i - tag) {
             Pattern pat = Pattern.compile(regEx);
             Matcher matcher = pat.matcher(s);
             matcher.find();
-            spell += (matcher.group() + " ");
+            spell.append(matcher.group()).append(",");
             tag = matcher.end() - matcher.start();
             tokenResult.add(s.substring(0, 1));
             s = s.substring(tag);
         }
+        return spell.toString();
+    }
 
-        return spell;
+    public static List<SplitResultItem> getSplitResultItems(String input) {
+        List<String> splitResult = new ArrayList<>(Arrays.asList(splitSpell(input).split(",")));
+        List<SplitResultItem> splitResultItems = new LinkedList<>();
+        Set<String> completeSpellSet = SimpleConfig.getCompleteSpellSet();
+        Set<String> simpleSpellSet = SimpleConfig.getSimpleSpellSet();
+        PinYinType type;
+        for (String tmp : splitResult) {
+            if (completeSpellSet.contains(tmp)) {
+                type = PinYinType.COMPLETESPELL;
+                //splitResultItems.add(new SplitResultItem(tmp, PinYinType.COMPLETESPELL));
+            } else if (simpleSpellSet.contains(tmp)) {
+                type = PinYinType.SIMPLESPELL;
+                //splitResultItems.add(new SplitResultItem(tmp, PinYinType.SIMPLESPELL));
+            } else {
+                type = PinYinType.OTHERSPELL;
+            }
+            splitResultItems.add(new SplitResultItem(tmp, type));
+        }
+        return splitResultItems;
     }
 
     public static void main(String[] args) {
         System.out.println(splitSpell("beijdxue"));
+        List<SplitResultItem> result = getSplitResultItems("bj");
+        for (SplitResultItem tmp : result) {
+            System.out.println("result - item:" + tmp.getPyItem() + ", type:" + tmp.getPyType());
+        }
+    }
+
+    private void init() {
+
     }
 
 }
