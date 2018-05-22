6e7f53c0f34a229069ce7d427f12cd0440cff2b9
luyunyyyyy <516537801@qq.com>
2018-05-22 11:37:46 +0800

    Factory 增加了管理 Instance 的数据文件的功能
    在初始化时把文件读取, 并序列化成对象, 再传递给 Instance

--- src/main/java/IMEngine/SimpleIMEngineFactory.java
+++ src/main/java/IMEngine/SimpleIMEngineFactory.java
@@ -1,11 +1,37 @@
 package IMEngine;
 
-public class SimpleIMEngineFactory implements IMEngineFactoryService {
-    public SimpleIMEngineFactory() {
+import javafx.util.Pair;
+import org.apache.log4j.Logger;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static Config.TrainConfig.SIMPLE_SPELL_DICT_FILE_NAME;
+import static Config.TrainConfig.TRAIN_DATA_OUTPUT_ROOT_PATH;
+
+public class SimpleIMEngineFactory implements IMEngineFactoryService {
+
+    Logger logger = Logger.getLogger(SimpleIMEngineFactory.class);
+    private HashMap<String, ArrayList<Pair<String, Double>>> simpleSpellDict;
+
+    public SimpleIMEngineFactory() throws IOException, ClassNotFoundException {
+        loadSpellDict();
     }
 
+    private void loadSpellDict() throws IOException, ClassNotFoundException {
+
+        FileInputStream fis = new FileInputStream(TRAIN_DATA_OUTPUT_ROOT_PATH + SIMPLE_SPELL_DICT_FILE_NAME);
+        ObjectInputStream ois = new ObjectInputStream(new BufferedInputStream(fis));
+        simpleSpellDict = (HashMap<String, ArrayList<Pair<String, Double>>>) ois.readObject();
+        logger.debug("loadSpellDict dict size : " + simpleSpellDict.size());
+    }
     public IMEngineInstance getIMEngineInstance() {
-        return new SimpleIMEngineInstance();
+        //return new SimpleIMEngineInstance();
+
+        return new SimpleIMEngineInstance(simpleSpellDict);
     }
 }
