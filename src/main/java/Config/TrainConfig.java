08828dd3a12c47f43d860f6133ae12ed8a809324
luyunyyyyy <516537801@qq.com>
2018-05-22 09:57:17 +0800

    保存训练数据相关的配置

---
+++ src/main/java/Config/TrainConfig.java
@@ -0,0 +1,6 @@
+package Config;
+
+public class TrainConfig {
+    public static final String TRAIN_DATA_ROOT_PATH = "C:\\Users\\ShiWanZhao\\Documents\\workplace\\MyIme\\MyIme\\src\\main\\resources\\source\\";
+    public static final String DICTWORD_FILE_NAME = "dictword.txt";
+}
