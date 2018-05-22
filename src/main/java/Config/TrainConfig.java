d5448f4940069f1bf6679036f7df200cf960183d
luyunyyyyy <516537801@qq.com>
2018-05-22 11:30:19 +0800

    增加了相关配置, 保存训练文件的输入输出路径

--- src/main/java/Config/TrainConfig.java
+++ src/main/java/Config/TrainConfig.java
@@ -1,6 +1,12 @@
 package Config;
 
 public class TrainConfig {
-    public static final String TRAIN_DATA_ROOT_PATH = "C:\\Users\\ShiWanZhao\\Documents\\workplace\\MyIme\\MyIme\\src\\main\\resources\\source\\";
+    public static final String TRAIN_DATA_INPUT_ROOT_PATH = "C:\\Users\\ShiWanZhao\\Documents\\workplace\\MyIme\\MyIme\\src\\main\\resources\\source\\";
+    public static final String TRAIN_DATA_OUTPUT_ROOT_PATH = "C:\\Users\\ShiWanZhao\\Documents\\workplace\\MyIme\\MyIme\\src\\main\\resources\\trained_data\\";
     public static final String DICTWORD_FILE_NAME = "dictword.txt";
+    public static final String SIMPLE_SPELL_DICT_FILE_NAME = "simpleSpellDict.dat";
+
+
+
+
 }
