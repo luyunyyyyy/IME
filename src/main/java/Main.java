c23d6e3b8876f33af5a7802d29dcaedfe05bf497
luyunyyyyy <516537801@qq.com>
2018-05-24 16:03:04 +0800

    整理了一下main函数
    增加了部分注释

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,40 +17,29 @@ public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         logger.info("调用 main ");
+
         Scanner sc = new Scanner(System.in);
         runFrontEnd(sc);
 
-
         logger.info("输入法运行结束 ");
-
     }
 
     private static void runFrontEnd(Scanner sc) throws IOException, ClassNotFoundException {
         Pattern pinyinPattern = Pattern.compile("^[A-Za-z]+$");
-        String input, result;
+        String input;
         FrontEndService frontEndService = init();
         List<CandidateItem> candidateWords;
         while (true) {
             System.out.print("input:");
             input = sc.next();
             logger.debug("input : " + input);
-
             if (input.equals("q")) {
                 logger.debug("quit");
                 break;
             }
-            /*
-            模块的功能入口
-            TODO 0516 下午的工作
-            1. 要把 result 抽象成一个类，以便于容纳候选词的数组 还要重写一下toString方法。然后提供一个通过索引访问单词的方法
-            2. 要考虑参数的配置问题 比如说候选词的大小
-             */
             if (pinyinPattern.matcher(input).matches()) {
                 logger.debug("input is pinyin");
-
-                //result = frontEndService.handleInputWords(input);
                 candidateWords = frontEndService.handleInputString(input);
-
             } else {
                 logger.debug("input is not pinyin");
                 break;
@@ -72,13 +61,12 @@ public class Main {
         }
     }
 
+    /*
+        修改该初始化的部分 可以更改instance的类别
+     */
     private static FrontEndService init() throws IOException, ClassNotFoundException {
         IMEngineFactoryService imEngineFactoryService = new SimpleIMEngineFactory();
         BackEndService backEndService = new SimpleBackEnd(imEngineFactoryService);
-        FrontEndService frontEndService = new SimpleFrontEnd(backEndService);
-        return frontEndService;
-    }
-    private static String setResult(String input) {
-        return input;
+        return new SimpleFrontEnd(backEndService);
     }
 }
